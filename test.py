dea01e0155a18dc3fea00f07659e76bbfb30458b
Dave <david.mathaga@student.moringaschool.com>
2021-10-25 06:45:10 +0300

    test to save cleanup and save many accs

--- test.py
+++ test.py
@@ -1,4 +1,48 @@
 import unittest
+from unittest import TestCase
+
 from User import User
 from User import Credential
 
+
+class Test(unittest, TestCase):
+
+    def setUp(self):
+        self.new_user = User("davymats", "mizza123XYZ")
+
+    def test_init(self):
+        self.assertEqual(self.new_user.user_name, 'davymats')
+        self.assertEqual(self.new_user.password, "mizza123XYZ")
+
+    def test_save_user(self):
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
+
+
+class TestCredentials(unittest.TestCase):
+
+    def setUp(self):
+        self.new_account = Credential('Instagram', 'David_Mathaga', '123psw')
+
+    def test_init(self):
+        self.assertEqual(self.new_account.account, 'Instagram')
+        self.assertEqual(self.new_account.user_name, 'David_Mathaga')
+        self.assertEqual(self.new_account.password, '123psw')
+
+    def save_acc(self):
+        self.new_account.save_account()
+        self.assertEqual(len(Credential.credential_list), 1)
+
+    def tearDown(self):
+        Credential.credential_list = []
+
+    def save_many_acc(self):
+        self.new_account.save_account()
+        test_acc = Credential('Facebook','makaupete','6qeq456')
+        test_acc.save_account()
+        self.assertEqual(len(Credential.credential_list),2)
+
+    def
+
+if __name__ == "__main__":
+    unittest.main()
