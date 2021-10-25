27ae749e44cd27af77afc51a6e4c1d41737cd49b
Dave <david.mathaga@student.moringaschool.com>
2021-10-25 05:45:04 +0300

    verify user code

--- User.py
+++ User.py
@@ -5,10 +5,22 @@ import string
 class User:
     user_list = []
 
-    def __init__(self, first_name, last_name, password):
-        self.first_name = first_name
-        self.last_name = last_name
+    def __init__(self, user_name, password):
+        self.user_name = user_name
         self.password = password
 
     def save_user(self):
         User.user_list.append(self)
+
+
+class Credential:
+    credential_list = []
+
+    @classmethod
+    def verify_user(cls, user_name, password):
+
+        current_user = ''
+        for user in User.user_list:
+            if user.user_name == user_name and user.password == password:
+                current_user == user.user_name
+            return current_user
