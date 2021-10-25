fc2284e1b3e9f9302cefbbe76d88e471a23fdd1e
Dave <david.mathaga@student.moringaschool.com>
2021-10-25 05:49:26 +0300

    delete and save acc code

--- User.py
+++ User.py
@@ -24,3 +24,14 @@ class Credential:
             if user.user_name == user_name and user.password == password:
                 current_user == user.user_name
             return current_user
+
+    def __init__(self, account, user_name, password):
+        self.account = account
+        self.user_name = user_name
+        self.password = password
+
+    def save_account(self):
+        Credential.credential_list.append(self)
+
+    def del_account(self):
+        Credential.credential_list.remove(self)
