b677ad303dc255f90070d0b17bb543be48506c75
Dave <david.mathaga@student.moringaschool.com>
2021-10-25 05:30:41 +0300

    save user fn

---
+++ User.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+
+class User:
+    user_list = []
+
+    def __init__(self, first_name, last_name, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+    def save_user(self):
+        User.user_list.append(self)
